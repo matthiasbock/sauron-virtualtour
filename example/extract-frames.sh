5d1c57d628c3a833be3b54b13318d6c08ee67914
matthias.bock <matthias.bock@hu-berlin.de>
2011-09-18 12:53:02 +0000

    extract frames script

---
+++ example/extract-frames.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for video in `ls *.MP4`; do
+	dir="$video.frames"
+	mkdir -p "$dir"
+	cd "$dir"
+	rm -f *.jpg
+	mplayer "../$video" -ao null -nosound -vo jpeg:quality=85:progressive -vf framestep=50
+	cd ..
+	done
